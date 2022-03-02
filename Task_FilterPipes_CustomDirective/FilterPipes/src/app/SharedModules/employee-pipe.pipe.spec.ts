402af836647955281f9258d1a33173c48bd6847f
vamshi <vamshichowdary130@gmail.com>
2022-03-02 09:09:44 +0530

    Removed Unnecessary files

--- Task_FilterPipes_CustomDirective/FilterPipes/src/app/SharedModules/employee-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EmployeePipePipe } from './employee-pipe.pipe';
-
-describe('EmployeePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new EmployeePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- Task_FilterPipes_CustomDirective/FilterPipes/src/app/customDirectives/navbarcolour-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NavbarcolourDirectiveDirective } from './navbarcolour-directive.directive';
-
-describe('NavbarcolourDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NavbarcolourDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
