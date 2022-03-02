57136a529ff4ad439fc0cfa325aed93904e94498
vamshi <vamshichowdary130@gmail.com>
2022-03-02 08:59:41 +0530

    Task-FilterPipe_CustomDirective

---
+++ Task_FilterPipes_CustomDirective/FilterPipes/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './Components/employees/employees.component';
+import { EmployeePipePipe } from './SharedModules/employee-pipe.pipe';
+import { NavbarcolourDirectiveDirective } from './customDirectives/navbarcolour-directive.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeePipePipe,
+    NavbarcolourDirectiveDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Task_FilterPipes_CustomDirective/FilterPipes/src/app/customDirectives/navbarcolour-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NavbarcolourDirectiveDirective } from './navbarcolour-directive.directive';
+
+describe('NavbarcolourDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NavbarcolourDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
